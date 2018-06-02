7a23e54f0cb26dd16bf8d38247568aaf16f87129
TarasMartynyuk <tarasmartynyuk22@gmail.com>
2018-06-02 20:44:23 +0300

    overlaping vector stream operator

--- super_computer.cpp
+++ super_computer.cpp
@@ -5,29 +5,48 @@
 #include <iterator>
 
 using namespace std;
-//region struct
+// region struct
 
 class Computation {
 public:
     int start;
     int end;
+    vector<Computation> overlapingComps;
 
     Computation& operator=(const Computation& other) = default;
 
+    size_t overlapNumber() { return overlapingComps.size(); }
+    void computeOverlapingComps() {
+
+    }
     bool overlaps(const Computation& other) {
         return other.end > start ||
                other.start < end;
     }
-};//endregion
-std::ostream& operator<<(std::ostream& os, const Computation& comp) {
-    os << "(" << comp.start << ", " << comp.end << ")";
+};
+
+ostream& writeSpan(ostream& os,
+                   const Computation& comp) {
+    os << "span: (" << comp.start << ", " << comp.end << ")";
     return os;
 }
+std::ostream& operator<<(std::ostream& os, const Computation& comp) {
+    os << "{\t";
+    writeSpan(os, comp) << "\n" << "\toverlaps: { ";
+
+    for (auto& overlapping : comp.overlapingComps) {
+        writeSpan(os, overlapping) << ",\n\t\t\t";
+    }
+    os << "}\n}";
+    return os;
+}
+
+
 struct StartComparator {
     bool operator() (const Computation& left, const Computation& right) {
         return left.start < right.start;
     }
-};
+};//endregion
 
 void readComputation(Computation& c) {
     int J;
@@ -43,33 +62,31 @@ vector<Computation> readComputations() {
     int length;     cin >> length;    cin.ignore();
     vector<Computation> comps(length);
 
-    for (size_t i = 0; i < length; ++i) {
-        readComputation(comps.at(i));
-    }
-
+    for_each(comps.begin(), comps.end(),
+             [](Computation& comp) { comp.computeOverlapingComps(); });
     return comps;
 }
 
 int main()
 {
-    vector<Computation> comps = readComputations();
-
-    ostream_iterator<Computation> output(cerr, " ");
-    std::copy(comps.begin(), comps.end(), output);
-
-
+//    vector<Computation> comps = readComputations();
 //
-//    cout << "answer" << endl;
+//    sort(comps.begin(), comps.end(), StartComparator());
+//
+//    for_each(comps.begin(), comps.end(), [](const Computation& comp) {
+//
+//    });
+//
+//    ostream_iterator<Computation> output(cerr, " ");
+//    std::copy(comps.begin(), comps.end(), output);
 
-//    int array[6] = { 1, 2, 3, 4, 5, 6 };
-//    std::vector<int> integers(array, array + 6);
-//    std::ostream_iterator<int> output(cout, " ");
-//
-//    integers.push_back(2);
-//    integers.push_back(3);
-//    integers.push_back(4);
-//
-//    cout << "Vector integers contains: ";
-//    std::copy(integers.begin(), integers.end(), output);
+    Computation comp;
+
+    comp.overlapingComps.push_back(Computation{ 4, 4});
+    comp.overlapingComps.push_back(Computation{ 4, 4 });
+
+
+    cout << comp;
+
     return 0;
 }
