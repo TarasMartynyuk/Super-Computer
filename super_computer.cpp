c70788a7b14cbc14c7dcc6fe36d0ea79ca106ccf
TarasMartynyuk <tarasmartynyuk22@gmail.com>
2018-06-02 21:59:14 +0300

    changed overlaping computations collection to unordered set, sorting on outputting

--- super_computer.cpp
+++ super_computer.cpp
@@ -3,25 +3,50 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <assert.h>
+#include <unordered_set>
 
 using namespace std;
 // region struct
+class Computation;
+namespace std {
+template<>
+struct hash<Computation> {
+    size_t operator()(const Computation& c) const noexcept;
+};
+}
+
 
 class Computation {
 public:
     int start;
     int end;
-    vector<Computation> overlapingComps;
+    unordered_set<Computation*> overlappingComps;
 
-    Computation& operator=(const Computation& other) = default;
-
-    size_t overlapNumber() { return overlapingComps.size(); }
-    void computeOverlapingComps() {
-
+    void addOverlapingComp(Computation* overlap) {
+        overlappingComps.insert(overlap);
     }
-    bool overlaps(const Computation& other) {
+
+    size_t overlapNumber()const { return overlappingComps.size(); }
+    bool overlaps(const Computation& other)const {
         return other.end > start ||
                other.start < end;
+    }
+    Computation& operator=(const Computation& other) = default;
+
+};
+
+size_t std::hash<Computation>::operator()(const Computation& c) const noexcept {
+    return (
+        (51 + std::hash<int>()(c.start)) * 51
+        + std::hash<int>()(c.end)
+    );
+}
+
+struct StartComparator {
+    bool operator()(const Computation& left,
+                    const Computation& right) {
+        return left.start < right.start;
     }
 };
 
@@ -34,19 +59,21 @@ std::ostream& operator<<(std::ostream& os, const Computation& comp) {
     os << "{\t";
     writeSpan(os, comp) << "\n" << "\toverlaps: { ";
 
-    for (auto& overlapping : comp.overlapingComps) {
-        writeSpan(os, overlapping) << ",\n\t\t\t";
+    vector<Computation*> comp_vec(comp.overlapNumber());
+    copy(comp.overlappingComps.begin(), comp.overlappingComps.end(),
+         comp_vec.begin());
+    sort(comp_vec.begin(), comp_vec.end(), [](
+        Computation* left, Computation* right) {
+        return left->start < right->start;
+    });
+
+    for (auto overlapping : comp_vec) {
+        writeSpan(os, *overlapping) << ",\n\t\t\t";
     }
     os << "}\n}";
     return os;
-}
-
-
-struct StartComparator {
-    bool operator() (const Computation& left, const Computation& right) {
-        return left.start < right.start;
-    }
-};//endregion
+}//endregion
+//region funcs
 
 void readComputation(Computation& c) {
     int J;
@@ -62,10 +89,42 @@ vector<Computation> readComputations() {
     int length;     cin >> length;    cin.ignore();
     vector<Computation> comps(length);
 
-    for_each(comps.begin(), comps.end(),
-             [](Computation& comp) { comp.computeOverlapingComps(); });
+    for_each(comps.begin(), comps.end(), readComputation);//[](Computation& comp) { readComputation(comp); });
     return comps;
 }
+void computeOverlapingCompsLeft(vector<Computation>& comp_vec, int index) {
+    Computation& this_comp = comp_vec.at(index);
+
+    while (index >= 0) {
+        if (this_comp.overlaps(comp_vec.at(index))) {
+            this_comp.addOverlapingComp(&comp_vec.at(index));
+            index--;
+        }
+        else {
+            break;
+        }
+    }
+}
+
+void computeOverlapingCompsRight(vector<Computation>& comp_vec, int index) {
+    Computation& this_comp = comp_vec.at(index);
+
+    while (index < comp_vec.size()) {
+        if (this_comp.overlaps(comp_vec.at(index))) {
+            this_comp.addOverlapingComp(&comp_vec.at(index));
+            index++;
+        }
+        else {
+            break;
+        }
+    }
+}
+void computeOverlapingComps(vector<Computation>& comp_vec, int this_index) {
+    // go left until zero index, add all comps that overlap, stop when first does not overlap
+    computeOverlapingCompsLeft(comp_vec, this_index);
+    computeOverlapingCompsRight(comp_vec, this_index);
+}
+//endregion
 
 int main()
 {
@@ -73,20 +132,23 @@ int main()
 //
 //    sort(comps.begin(), comps.end(), StartComparator());
 //
-//    for_each(comps.begin(), comps.end(), [](const Computation& comp) {
-//
-//    });
+//    for (int i = 0; i < comps.size(); ++i) {
+//        computeOverlapingComps(comps, i);
+//    }
 //
 //    ostream_iterator<Computation> output(cerr, " ");
 //    std::copy(comps.begin(), comps.end(), output);
+//
+//    return 0;
 
     Computation comp;
 
-    comp.overlapingComps.push_back(Computation{ 4, 4});
-    comp.overlapingComps.push_back(Computation{ 4, 4 });
 
+    Computation c1 { 14, 4 };
+    Computation c2{ 4, 5 };
+
+    comp.addOverlapingComp(&c1);
+    comp.addOverlapingComp(&c2);
 
     cout << comp;
-
-    return 0;
 }
